e157c5fcec45a9e2f20e426a507898937652dd80
Albert Arrebola Sans <104431726+albertarrebola08@users.noreply.github.com>
2024-06-10 15:25:31 +0200

    Update main2.py

--- Classe 6 - Srapping + Spotify API/main2.py
+++ Classe 6 - Srapping + Spotify API/main2.py
@@ -8,8 +8,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 import json
-api_client_id = "2d24e72bccfc459d8c6eb1408f954097"
-api_client_secret = "29126da8bfd742a39389cb3a03766b64"
+api_client_id = ""
+api_client_secret = ""
 
 # Configurar el cliente de Spotify
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(api_client_id, api_client_secret))
